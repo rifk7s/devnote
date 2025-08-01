993613eaf1d41c353ca3e8bb5d302db76b79c6ed
M. Rifki Paranrengi <rifkip1337@gmail.com>
2025-08-01 22:41:19 +0800

    (#5) finished app authenthication
    [+] feat(shared): create reset password form and logic
    
    [+] feat(app): implement forogt passowrd form"
    
    [+] fix(server): improving functionality and logic form
    
    [+] fix: streamline email logic for verif and pw reset

--- components/forms/reset-password-form.tsx
+++ components/forms/reset-password-form.tsx
@@ -1,20 +1,20 @@
-"use client"
+"use client";
 
-import { z } from "zod"
+import { z } from "zod";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 
 import {
   Form,
@@ -23,46 +23,58 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { signInUser } from "@/server/users"
-import { useState } from "react"
-import { toast } from "sonner"
-import { Loader2 } from "lucide-react"
-import Link from "next/link"
-import { useRouter } from "next/navigation"
-
+} from "@/components/ui/form";
+import { signInUser } from "@/server/users";
+import { useState } from "react";
+import { toast } from "sonner";
+import { Loader2 } from "lucide-react";
+import Link from "next/link";
+import { useRouter, useSearchParams } from "next/navigation";
+import { authClient } from "@/lib/auth-client";
 
 const formSchema = z.object({
-  email: z.email(),
   password: z.string().min(8),
-})
+  confirmPassword: z.string().min(8),
+});
 
-export function LoginForm({
+export function ResetPasswordForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
-  const router = useRouter()
-  
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
   const [isLoading, setIsLoading] = useState(false);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      email: "",
       password: "",
+      confirmPassword: "",
     },
-  })
+  });
 
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
       setIsLoading(true);
-      const response = await signInUser(values.email, values.password);
-      if (response.success) {
-        toast.success(response.message);
-        router.push("/dashboard");
+
+      if (values.password !== values.confirmPassword) {
+        toast.error("Passwords do not match.");
+        return;
+      }
+      const { error } = await authClient.resetPassword({
+        newPassword: values.password,
+        token: token ?? "",
+      });
+
+      if (!error) {
+        toast.success("Password reset successfully.");
+        router.push("/login");
       } else {
-        toast.error(response.message);
+        toast.error(error.message);
       }
     } catch (error) {
       console.error(error);
@@ -73,12 +85,11 @@ export function LoginForm({
 
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
-
       <Card>
         <CardHeader>
-          <CardTitle>Login to your account</CardTitle>
+          <CardTitle>Reset your password</CardTitle>
           <CardDescription>
-            Enter your email below to login to your account
+            Enter your new password below to reset your password
           </CardDescription>
         </CardHeader>
         <CardContent>
@@ -88,40 +99,36 @@ export function LoginForm({
                 <div className="grid gap-3">
                   <FormField
                     control={form.control}
-                    name="email"
+                    name="password"
                     render={({ field }) => (
                       <FormItem>
-                        <FormLabel>Email</FormLabel>
+                        <FormLabel>Password</FormLabel>
                         <FormControl>
-                          <Input placeholder="m@example.com" {...field} />
+                          <Input
+                            type="password"
+                            placeholder="••••••••"
+                            {...field}
+                          />
                         </FormControl>
-
                         <FormMessage />
                       </FormItem>
                     )}
                   />
                 </div>
                 <div className="grid gap-3">
-
                   <FormField
                     control={form.control}
-                    name="password"
+                    name="confirmPassword"
                     render={({ field }) => (
                       <FormItem>
-                        <div className="flex items-center">
-                          <FormLabel>Password</FormLabel>
-                          <Link
-                          href="/forgot-password"
-                            className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                          >
-                            Forgot your password?
-                          </Link>
-                        </div>
-
+                        <FormLabel>Confirm Password</FormLabel>
                         <FormControl>
-                          <Input type="password" placeholder="••••••••" {...field} />
+                          <Input
+                            type="password"
+                            placeholder="••••••••"
+                            {...field}
+                          />
                         </FormControl>
-
                         <FormMessage />
                       </FormItem>
                     )}
@@ -129,11 +136,13 @@ export function LoginForm({
                 </div>
                 <div className="flex flex-col gap-3">
                   <Button type="submit" className="w-full" disabled={isLoading}>
-                    {isLoading ? <Loader2 className="size-4 animate-spin" /> : "Login"}
+                    {isLoading ? (
+                      <Loader2 className="size-4 animate-spin" />
+                    ) : (
+                      "Reset Password"
+                    )}
                   </Button>
-                  <Button variant="outline" className="w-full">
-                    Login with Google
-                  </Button>
+                  
                 </div>
               </div>
               <div className="mt-4 text-center text-sm">
@@ -147,5 +156,5 @@ export function LoginForm({
         </CardContent>
       </Card>
     </div>
-  )
+  );
 }
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,4 @@
+import PasswordResetEmail from "@/components/emails/reset-email";
 import VerificationEmail from "@/components/emails/verification-email";
 import { db } from "@/db/drizzle"; // your drizzle instance
 import { schema } from "@/db/schema";
@@ -10,11 +11,11 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const auth = betterAuth({
   emailVerification: {
-    sendVerificationEmail: async ({ user, url, token }, request) => {
-      const { data, error } = await resend.emails.send({
-        from: 'DevNote <onboarding@resend.dev>', //change this later, when domain buyed
+    sendVerificationEmail: async ({ user, url }) => {
+      await resend.emails.send({
+        from: "DevNote <onboarding@resend.dev>", //change this later, when domain buyed
         to: [user.email],
-        subject: 'Verify your email address',
+        subject: "Verify your email address",
         react: VerificationEmail({ userName: user.name, verificationUrl: url }),
       });
     },
@@ -22,10 +23,19 @@ export const auth = betterAuth({
   },
   emailAndPassword: {
     enabled: true,
+    sendResetPassword: async ({ user, url }) => {
+      await resend.emails.send({
+        from: "DevNote <onboarding@resend.dev>",
+        to: user.email,
+        subject: "Reset your password",
+        react: PasswordResetEmail({ userName: user.name, resetUrl: url, requestTime: new Date().toLocaleString() }),
+      });
+    },
+    
   },
   database: drizzleAdapter(db, {
     provider: "pg",
-    schema
+    schema,
   }),
-  plugins: [nextCookies()]
+  plugins: [nextCookies()],
 });
