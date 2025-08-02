4f77475456292159819fe86ebf6a80b205f58666
RIFKY <rifkip1337@gmail.com>
2025-08-02 15:40:18 +0800

    feat(server): small commits ; need rest

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,21 @@
 import { Logout } from "@/components/logout";
 import { PageWrapper } from "@/components/page-wrapper";
+import { getNotebooks } from "@/server/notebooks";
 
-export default function Page() {
-    return (
-        <PageWrapper breadcrumbs={[{ label: "Dashboard", href: "/dashboard" }]}>
-            <h1>Dashboard</h1>
-            <Logout />
-            {/* Add more dashboard content here */}
-        </PageWrapper>
-    );
+export default async function Page() {
+  const notebooks = await getNotebooks();
+  return (
+    <PageWrapper breadcrumbs={[{ label: "Dashboard", href: "/dashboard" }]}>
+      <h1>Dashboard</h1>
+      {notebooks.success &&
+        notebooks?.notebooks?.map((notebook) => (
+          <div key={notebook.id}>{notebook.name}</div>
+        ))}
+      {notebooks.success && notebooks?.notebooks?.length === 0 && (
+        <div>No notebooks found</div>
+      )}
+      <Logout />
+      {/* Add more dashboard content here */}
+    </PageWrapper>
+  );
 }
