2bb043a98c003b6e953fd4d182d87f8e95243925
RIFKY <rifkip1337@gmail.com>
2025-08-03 23:58:56 +0800

    integrate Nuqs for state management and add SidebarData component for dynamic sidebar rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NuqsAdapter>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -38,6 +40,7 @@ export default function RootLayout({
           <Toaster />
           {children}
         </ThemeProvider>
+        </NuqsAdapter>
       </body>
     </html>
   );
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -22,6 +22,7 @@ import {
 } from "@/components/ui/sidebar";
 import { getNotebooks } from "@/server/notebooks";
 import Image from "next/image";
+import { SidebarData } from "./sidebar-data";
 
 export async function AppSidebar({
   ...props
@@ -58,44 +59,7 @@ export async function AppSidebar({
         <SearchForm />
       </SidebarHeader>
       <SidebarContent className="gap-0">
-        {/* We create a collapsible SidebarGroup for each parent. */}
-        {data.navMain.map((item) => (
-          <Collapsible
-            key={item.title}
-            title={item.title}
-            defaultOpen
-            className="group/collapsible"
-          >
-            <SidebarGroup>
-              <SidebarGroupLabel
-                asChild
-                className="group/label text-sidebar-foreground hover:bg-sidebar-accent hover:text-sidebar-accent-foreground text-sm"
-              >
-                <CollapsibleTrigger>
-                  {item.title}{" "}
-                  {item.items.length > 0 && (
-                    <ChevronRight className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-90" />
-                  )}
-                </CollapsibleTrigger>
-              </SidebarGroupLabel>
-              <CollapsibleContent>
-                <SidebarGroupContent>
-                  <SidebarMenu>
-                    {item.items.map((item) => (
-                      <SidebarMenuItem key={item.title}>
-                        <SidebarMenuButton asChild isActive={item.isActive}>
-                          <a href={item.url}>
-                            <File />
-                            {item.title}</a>
-                        </SidebarMenuButton>
-                      </SidebarMenuItem>
-                    ))}
-                  </SidebarMenu>
-                </SidebarGroupContent>
-              </CollapsibleContent>
-            </SidebarGroup>
-          </Collapsible>
-        ))}
+        <SidebarData data={data} />
       </SidebarContent>
       <SidebarRail />
     </Sidebar>
